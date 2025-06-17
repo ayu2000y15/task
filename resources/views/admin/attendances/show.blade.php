2ccd056880064b8fb0f437e3b8b9aafbd1be4384
ayu2000y15 <ayu2000y15@nasystem.net>
2025-06-18 08:11:49 +0900

    　　日付：2025/06/18 　変更者：野口 変更内容：微調整

--- resources/views/admin/attendances/show.blade.php
+++ resources/views/admin/attendances/show.blade.php
@@ -166,13 +166,23 @@
                                             <table class="min-w-full text-sm">
                                                 <thead class="border-b-2 border-gray-300 dark:border-gray-600">
                                                     <tr>
-                                                        <th class="py-2 px-3 text-left">案件</th><th class="py-2 px-3 text-left">工程</th><th class="py-2 px-3 text-left">開始</th><th class="py-2 px-3 text-left">終了</th><th class="py-2 px-3 text-left">作業時間</th>
+                                                        <th class="py-2 px-3 text-left">案件</th>
+                                                        <th class="py-2 px-3 text-left">キャラクター</th>
+                                                        <th class="py-2 px-3 text-left">工程</th>
+                                                        <th class="py-2 px-3 text-left">開始</th>
+                                                        <th class="py-2 px-3 text-left">終了</th>
+                                                        <th class="py-2 px-3 text-left">作業時間</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
                                                     @forelse($logs as $log)
                                                         <tr class="border-b dark:border-gray-600/50">
-                                                            <td class="py-2 px-3">{{ $log->task->project->title ?? '-' }}</td><td class="py-2 px-3">{{ $log->task->name ?? '-' }}</td><td class="py-2 px-3">{{ $log->start_time->format('H:i') }}</td><td class="py-2 px-3">{{ optional($log->end_time)->format('H:i') }}</td><td class="py-2 px-3">{{ gmdate('H:i:s', $log->effective_duration) }}</td>
+                                                            <td class="py-2 px-3">{{ $log->task->project->title ?? '-' }}</td>
+                                                            <td class="py-2 px-3">{{ optional($log->task->character)->name ?? '-' }}</td>
+                                                            <td class="py-2 px-3">{{ $log->task->name ?? '-' }}</td>
+                                                            <td class="py-2 px-3">{{ $log->start_time->format('H:i') }}</td>
+                                                            <td class="py-2 px-3">{{ optional($log->end_time)->format('H:i') }}</td>
+                                                            <td class="py-2 px-3">{{ gmdate('H:i:s', $log->effective_duration) }}</td>
                                                         </tr>
                                                     @empty
                                                         <tr><td colspan="5" class="py-4 px-3 text-center text-gray-500">この日の作業ログはありません。</td></tr>
