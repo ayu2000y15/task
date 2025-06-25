4449ee7363f1e5aeeab1542fe830672a0770b23a
ayu2000y15 <ayu2000y15@nasystem.net>
2025-06-25 10:54:21 +0900

    　　日付：2025/06/25 　変更者：野口 変更内容：案件の工程横スクロール

--- resources/views/projects/partials/projects-task-table.blade.php
+++ resources/views/projects/partials/projects-task-table.blade.php
@@ -3,7 +3,7 @@
     $formIdentifier = 'project';
 @endphp
 
-<div id="task-list-container-{{ $tableId }}">
+<div id="task-list-container-{{ $tableId }}" class="overflow-x-auto overflow-y-auto max-h-[60vh]">
     <div class="mb-4">
         @php
             $hideCompletedParams = request()->query();
