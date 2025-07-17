c143de283975528d09e810e0a2ee2801f01f8920
ayu2000y15 <ayu2000y15@nasystem.net>
2025-07-18 01:58:04 +0900

    　　日付：2025/07/18 　変更者：野口 変更内容：外部フォームの画像選択し追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -710,11 +710,11 @@
                                     @can('viewAny', App\Models\FormFieldDefinition::class)
                                         <a href="{{ route('admin.form-definitions.index') }}" class="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600">カスタム項目管理</a>
                                     @endcan
-                                    @can('viewAny', App\Models\BoardPostType::class)
-                                        <a href="{{ route('admin.board-post-types.index') }}" class="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600">投稿タイプ管理</a>
-                                    @endcan
                                     @can('viewAny', App\Models\FormFieldDefinition::class)
                                         <a href="{{ route('admin.form-categories.index') }}" class="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600">外部フォーム管理</a>
+                                    @endcan
+                                    @can('viewAny', App\Models\BoardPostType::class)
+                                        <a href="{{ route('admin.board-post-types.index') }}" class="block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600">投稿タイプ管理</a>
                                     @endcan
                                 </div>
                             </div>
